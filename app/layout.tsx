661917cc45ce8f5320fe291855e01bf2d9406db9
Aleksander Podobnik <aleksander.podobnik@gmail.com>
2025-12-28 20:00:02 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any"></link>
+      </head>
+
       <body
         className={`${nunito.className} bg-gray-50 text-gray-950 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
